a5a721d82a0a2c06a856737611444b987f4c93b5
Jérôme Avond <jerome@rachetjay.fr>
2021-10-12 09:10:42 +0200

    Test envoi mail

--- drinkmanager/drink/admin.py
+++ drinkmanager/drink/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
+from django.core.mail import send_mail
+
+#	def user_send_mail
+#	send_mail(
+#	    'Subject here',
+#	    'Here is the message.',
+#	    'from@example.com',
+#	    ['to@example.com'],
+#	    fail_silently=False,
+#	)
 
 # Register your models here.
 from .models import Drink, Stock, Consumption
