21b123ef2559fac28f64afcf08700c32ecc16176
jadjay <jerome.avond@alolise.org>
2022-09-29 00:24:17 +0200

    Amelioration Docker : externaliser database, externaliser configuration, USER drink, tests

--- drinkmanager/drinkmanager/default_settings.py
+++ drinkmanager/drinkmanager/default_settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'drinkmanager.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join('/database', 'db.${DJANGO_PROJECT_NAME}.sqlite3'),
     }
 }
 
--- drinkmanager/execution_file.sh
+++ drinkmanager/execution_file.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
 
-### VARIABLES A EDITER
-export DJANGO_SUPERUSER_PASSWORD="MONPASSWORDADMIN"
-export DJANGO_SUPERUSER_USERNAME="MONUSERADMIN"
-export DJANGO_SUPERUSER_EMAIL="MONMAIL@ADMINISTRAT.EU"
-export DJANGO_SECRETKEY="\"SECRETKEYTOGENERATE\""
-export DJANGO_DEBUG=True
-export DJANGO_EMAIL_HOST="''"
-export DJANGO_EMAIL_PORT=589
-export DJANGO_EMAIL_HOST_USER="''"
-export DJANGO_EMAIL_HOST_PASSWORD="''"
-export DJANGO_EMAIL_USE_TLS=True
+#### VARIABLES A EDITER
+#export DJANGO_SUPERUSER_PASSWORD="azerty/42"
+#export DJANGO_SUPERUSER_USERNAME="jadjay"
+#export DJANGO_SUPERUSER_EMAIL="jadjay@rachetjay.fr"
+#export DJANGO_DEBUG=True
+#export DJANGO_EMAIL_HOST="''"
+#export DJANGO_EMAIL_PORT=589
+#export DJANGO_EMAIL_HOST_USER="''"
+#export DJANGO_EMAIL_HOST_PASSWORD="''"
+#export DJANGO_EMAIL_USE_TLS=True
 
 
+export DJANGO_SECRETKEY="\"$(date | sha1sum | sed 's/ .*//')\""
+cd drinkmanager
 cat drinkmanager/default_settings.py | envsubst > drinkmanager/settings.py
 
 #cat drinkmanager/settings.py
 
+echo "########### MIGRATION "
 python manage.py makemigrations
 python manage.py migrate
+echo "########### MIGRATION "
 
-python manage.py createsuperuser --noinput
+python manage.py createsuperuser --noinput --email $DJANGO_SUPERUSER_EMAIL
 
 python manage.py runserver 0.0.0.0:8001
 
